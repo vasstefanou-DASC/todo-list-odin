66c9b9dd6d4ecff036d4cc8d95d8daf26c776314
Vasileios Stefanou <vas.stefanou@gmail.com>
2026-06-26 16:11:21 +0300

    Webpack setup

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    entry: {
+        app: "./src/index.js",
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resourse",
+            },
+        ],
+    },
+};
